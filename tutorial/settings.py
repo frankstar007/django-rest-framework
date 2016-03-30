6415bf3da9d3df881f966a291a56be1ac580d29f
frankstar <568106896@qq.com>
2016-03-30 10:11:15 +0800

    创建模型snippets

--- tutorial/settings.py
+++ tutorial/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'snippets.apps.SnippetsConfig',
+    'snippets',
     'rest_framework'
 )
 
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'tutorial.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'tutorial',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
